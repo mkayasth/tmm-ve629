71c25a7eacb964425cff98899af315bfc0617947
Kayastha <meritkayastha@gmail.com>
2026-07-13 17:55:12 -0500

    line graphs.

--- playground.R
+++ playground.R
@@ -159,3 +159,599 @@ res_alt2 <- calc_singscore_auc(
   down_genes = c("ADRA1A", "TERT"))
 
 res_alt2$auc
+
+###################################################################################
+###################################################################################
+###################################################################################
+
+#z Line graph -- genes TMM-ve.
+library(readxl)
+library(tidyverse)
+
+df <- read_excel("tmmSignature.xlsx")
+
+# removing pivot genes.
+df_plot <- df %>%
+  filter(method == "SINGSCORE")
+
+# preserving order.
+df_plot$Genes <- factor(df_plot$Genes)
+
+
+# auc plot.
+pivot_n <- 5
+auc_long <- df_plot %>%
+  dplyr::select(Genes, `auc_training`, `auc_testing`) %>%
+  pivot_longer(cols = -Genes, names_to = "Dataset", values_to = "AUC") %>%
+  mutate(AUC = as.numeric(AUC))
+
+auc_long <- auc_long %>%
+  mutate(
+    AUC = as.numeric(AUC)
+  ) %>%
+  filter(!is.na(AUC))
+
+auc_long <- auc_long %>%
+  group_by(Dataset) %>%
+  mutate(
+    Added_Genes = row_number(),
+    Signature_Size = pivot_n + Added_Genes
+  ) %>%
+  ungroup()
+
+pivot_labels <- c(
+  "SUV39H1", "FMO5", "KCTD21", "CAB39L", "PLCXD1"
+)
+
+selection_labels <- auc_long %>%
+  filter(Dataset == unique(Dataset)[1]) %>%
+  arrange(Signature_Size) %>%
+  pull(Genes) %>%
+  as.character()
+
+all_labels <- c(pivot_labels, selection_labels)
+
+all_breaks <- c(
+  1:pivot_n,
+  sort(unique(auc_long$Signature_Size))
+)
+
+all_breaks <- seq_along(all_labels)
+
+highlight_genes <- c("GRHL2")
+
+vline_df <- auc_long %>%
+  filter(Genes %in% highlight_genes) %>%
+  distinct(Genes, Signature_Size)
+
+
+ggplot(
+  auc_long,
+  aes(
+    x = Signature_Size,
+    y = AUC,
+    color = Dataset,
+    group = Dataset
+  )
+) +
+  
+  # Pivot region
+  geom_rect(
+    xmin = 0,
+    xmax = pivot_n,
+    ymin = -Inf,
+    ymax = Inf,
+    fill = "grey90",
+    color = "grey50",
+    linetype = "dashed",
+    linewidth = 0.8,
+    alpha = 0.5,
+    inherit.aes = FALSE
+  ) +
+  
+  # Training/Test curves
+  geom_line(linewidth = 1.3) +
+  geom_point(size = 2) +
+  
+  # Labels
+  annotate(
+    "text",
+    x = pivot_n/2,
+    y = 0.92,
+    label = "Pivot Genes",
+    fontface = "bold",
+    size = 4
+  ) +
+  
+  scale_x_continuous(
+    breaks = all_breaks,
+    labels = all_labels,
+    limits = c(1, max(all_breaks))
+  )+
+  
+  scale_y_continuous(
+    limits = c(0.50, 1.00),
+    breaks = seq(0.6, 1.00, 0.1)
+  ) +
+  guides(
+    color = guide_legend(
+      override.aes = list(
+        linetype = 0,   # removes line
+        shape = 16,
+        linewidth = 0
+      )
+    )
+  ) +
+  
+  labs(
+    x = "Total Signature Size",
+    y = "Harmonic Mean AUC",
+    color = NULL
+  ) +
+  
+  theme_classic(base_size = 16) +
+  
+  theme(
+    legend.position = "right",
+    axis.title = element_text(
+      face = "bold"),
+    axis.text.x = element_text(
+      angle = 45,
+      hjust = 1
+    )
+    
+  ) +
+  
+  geom_vline(
+    data = vline_df,
+    aes(xintercept = Signature_Size),
+    linetype = "dashed"
+  )
+
+###################################################################################
+###################################################################################
+###################################################################################
+
+#z Line graph -- genes ALT.
+library(readxl)
+library(tidyverse)
+
+df <- read_excel("altSignature.xlsx")
+
+# removing pivot genes.
+df_plot <- df %>%
+  filter(method == "SINGSCORE")
+
+# preserving order.
+df_plot$Genes <- factor(df_plot$Genes)
+
+
+# auc plot.
+pivot_n <- 5
+auc_long <- df_plot %>%
+  dplyr::select(Genes, `auc_training`, `auc_testing`) %>%
+  pivot_longer(cols = -Genes, names_to = "Dataset", values_to = "AUC") %>%
+  mutate(AUC = as.numeric(AUC))
+
+auc_long <- auc_long %>%
+  mutate(
+    AUC = as.numeric(AUC)
+  ) %>%
+  filter(!is.na(AUC))
+
+auc_long <- auc_long %>%
+  group_by(Dataset) %>%
+  mutate(
+    Added_Genes = row_number(),
+    Signature_Size = pivot_n + Added_Genes
+  ) %>%
+  ungroup()
+
+pivot_labels <- c(
+  "LMNTD2", "LINC01783", "CCNB3", "OR56A3", "ADRA1A"
+)
+
+selection_labels <- auc_long %>%
+  filter(Dataset == unique(Dataset)[1]) %>%
+  arrange(Signature_Size) %>%
+  pull(Genes) %>%
+  as.character()
+
+all_labels <- c(pivot_labels, selection_labels)
+
+all_breaks <- c(
+  1:pivot_n,
+  sort(unique(auc_long$Signature_Size))
+)
+
+all_breaks <- seq_along(all_labels)
+
+highlight_genes <- c("DNAJB13")
+
+vline_df <- auc_long %>%
+  filter(Genes %in% highlight_genes) %>%
+  distinct(Genes, Signature_Size)
+
+
+ggplot(
+  auc_long,
+  aes(
+    x = Signature_Size,
+    y = AUC,
+    color = Dataset,
+    group = Dataset
+  )
+) +
+  
+  # Pivot region
+  geom_rect(
+    xmin = 0,
+    xmax = pivot_n,
+    ymin = -Inf,
+    ymax = Inf,
+    fill = "grey90",
+    color = "grey50",
+    linetype = "dashed",
+    linewidth = 0.8,
+    alpha = 0.5,
+    inherit.aes = FALSE
+  ) +
+  
+  # Training/Test curves
+  geom_line(linewidth = 1.3) +
+  geom_point(size = 2) +
+  
+  # Labels
+  annotate(
+    "text",
+    x = pivot_n/2,
+    y = 0.92,
+    label = "Pivot Genes",
+    fontface = "bold",
+    size = 4
+  ) +
+  
+  scale_x_continuous(
+    breaks = all_breaks,
+    labels = all_labels,
+    limits = c(1, max(all_breaks))
+  )+
+  
+  scale_y_continuous(
+    limits = c(0.50, 1.00),
+    breaks = seq(0.6, 1.00, 0.1)
+  ) +
+  guides(
+    color = guide_legend(
+      override.aes = list(
+        linetype = 0,   # removes line
+        shape = 16,
+        linewidth = 0
+      )
+    )
+  ) +
+  
+  labs(
+    x = "Total Signature Size",
+    y = "Harmonic Mean AUC",
+    color = NULL
+  ) +
+  
+  theme_classic(base_size = 16) +
+  
+  theme(
+    legend.position = "right",
+    axis.title = element_text(
+      face = "bold"),
+    axis.text.x = element_text(
+      angle = 45,
+      hjust = 1
+    )
+    
+  ) +
+  
+  geom_vline(
+    data = vline_df,
+    aes(xintercept = Signature_Size),
+    linetype = "dashed"
+  )
+
+###################################################################################
+###################################################################################
+###################################################################################
+
+#z Line graph -- upregulated genes TMM-ve.
+library(readxl)
+library(tidyverse)
+
+df <- read_excel("tmmSignatureUpregulated.xlsx")
+
+# removing pivot genes.
+df_plot <- df %>%
+  filter(method == "SINGSCORE")
+
+# preserving order.
+df_plot$Genes <- factor(df_plot$Genes)
+
+
+# auc plot.
+pivot_n <- 5
+auc_long <- df_plot %>%
+  dplyr::select(Genes, `auc_training`, `auc_testing`) %>%
+  pivot_longer(cols = -Genes, names_to = "Dataset", values_to = "AUC") %>%
+  mutate(AUC = as.numeric(AUC))
+
+auc_long <- auc_long %>%
+  mutate(
+    AUC = as.numeric(AUC)
+  ) %>%
+  filter(!is.na(AUC))
+
+auc_long <- auc_long %>%
+  group_by(Dataset) %>%
+  mutate(
+    Added_Genes = row_number(),
+    Signature_Size = pivot_n + Added_Genes
+  ) %>%
+  ungroup()
+
+pivot_labels <- c(
+  "FMO5", "KCTD21", "FAXDC2", "CAB39L", "MYORG"
+)
+
+selection_labels <- auc_long %>%
+  filter(Dataset == unique(Dataset)[1]) %>%
+  arrange(Signature_Size) %>%
+  pull(Genes) %>%
+  as.character()
+
+all_labels <- c(pivot_labels, selection_labels)
+
+all_breaks <- c(
+  1:pivot_n,
+  sort(unique(auc_long$Signature_Size))
+)
+
+all_breaks <- seq_along(all_labels)
+
+highlight_genes <- c("GRHL2")
+
+vline_df <- auc_long %>%
+  filter(Genes %in% highlight_genes) %>%
+  distinct(Genes, Signature_Size)
+
+
+ggplot(
+  auc_long,
+  aes(
+    x = Signature_Size,
+    y = AUC,
+    color = Dataset,
+    group = Dataset
+  )
+) +
+  
+  # Pivot region
+  geom_rect(
+    xmin = 0,
+    xmax = pivot_n,
+    ymin = -Inf,
+    ymax = Inf,
+    fill = "grey90",
+    color = "grey50",
+    linetype = "dashed",
+    linewidth = 0.8,
+    alpha = 0.5,
+    inherit.aes = FALSE
+  ) +
+  
+  # Training/Test curves
+  geom_line(linewidth = 1.3) +
+  geom_point(size = 2) +
+  
+  # Labels
+  annotate(
+    "text",
+    x = pivot_n/2,
+    y = 0.92,
+    label = "Pivot Genes",
+    fontface = "bold",
+    size = 4
+  ) +
+  
+  scale_x_continuous(
+    breaks = all_breaks,
+    labels = all_labels,
+    limits = c(1, max(all_breaks))
+  )+
+  
+  scale_y_continuous(
+    limits = c(0.50, 1.00),
+    breaks = seq(0.6, 1.00, 0.1)
+  ) +
+  guides(
+    color = guide_legend(
+      override.aes = list(
+        linetype = 0,   # removes line
+        shape = 16,
+        linewidth = 0
+      )
+    )
+  ) +
+  
+  labs(
+    x = "Total Signature Size",
+    y = "Harmonic Mean AUC",
+    color = NULL
+  ) +
+  
+  theme_classic(base_size = 16) +
+  
+  theme(
+    legend.position = "right",
+    axis.title = element_text(
+      face = "bold"),
+    axis.text.x = element_text(
+      angle = 45,
+      hjust = 1
+    )
+    
+  ) +
+  
+  geom_vline(
+    data = vline_df,
+    aes(xintercept = Signature_Size),
+    linetype = "dashed"
+  )
+
+###################################################################################
+###################################################################################
+###################################################################################
+
+#z Line graph -- upregulated genes ALT.
+library(readxl)
+library(tidyverse)
+
+df <- read_excel("altSignatureUpregulated.xlsx")
+
+# removing pivot genes.
+df_plot <- df %>%
+  filter(method == "SINGSCORE")
+
+# preserving order.
+df_plot$Genes <- factor(df_plot$Genes)
+
+
+# auc plot.
+pivot_n <- 5
+auc_long <- df_plot %>%
+  dplyr::select(Genes, `auc_training`, `auc_testing`) %>%
+  pivot_longer(cols = -Genes, names_to = "Dataset", values_to = "AUC") %>%
+  mutate(AUC = as.numeric(AUC))
+
+auc_long <- auc_long %>%
+  mutate(
+    AUC = as.numeric(AUC)
+  ) %>%
+  filter(!is.na(AUC))
+
+auc_long <- auc_long %>%
+  group_by(Dataset) %>%
+  mutate(
+    Added_Genes = row_number(),
+    Signature_Size = pivot_n + Added_Genes
+  ) %>%
+  ungroup()
+
+pivot_labels <- c(
+  "LINC01783", "LMNTD2", "CCNB3", "OR56A3", "SLCO1A2"
+)
+
+selection_labels <- auc_long %>%
+  filter(Dataset == unique(Dataset)[1]) %>%
+  arrange(Signature_Size) %>%
+  pull(Genes) %>%
+  as.character()
+
+all_labels <- c(pivot_labels, selection_labels)
+
+all_breaks <- c(
+  1:pivot_n,
+  sort(unique(auc_long$Signature_Size))
+)
+
+all_breaks <- seq_along(all_labels)
+
+highlight_genes <- c("F8")
+
+vline_df <- auc_long %>%
+  filter(Genes %in% highlight_genes) %>%
+  distinct(Genes, Signature_Size)
+
+
+ggplot(
+  auc_long,
+  aes(
+    x = Signature_Size,
+    y = AUC,
+    color = Dataset,
+    group = Dataset
+  )
+) +
+  
+  # Pivot region
+  geom_rect(
+    xmin = 0,
+    xmax = pivot_n,
+    ymin = -Inf,
+    ymax = Inf,
+    fill = "grey90",
+    color = "grey50",
+    linetype = "dashed",
+    linewidth = 0.8,
+    alpha = 0.5,
+    inherit.aes = FALSE
+  ) +
+  
+  # Training/Test curves
+  geom_line(linewidth = 1.3) +
+  geom_point(size = 2) +
+  
+  # Labels
+  annotate(
+    "text",
+    x = pivot_n/2,
+    y = 0.92,
+    label = "Pivot Genes",
+    fontface = "bold",
+    size = 4
+  ) +
+  
+  scale_x_continuous(
+    breaks = all_breaks,
+    labels = all_labels,
+    limits = c(1, max(all_breaks))
+  )+
+  
+  scale_y_continuous(
+    limits = c(0.50, 1.00),
+    breaks = seq(0.6, 1.00, 0.1)
+  ) +
+  guides(
+    color = guide_legend(
+      override.aes = list(
+        linetype = 0,   # removes line
+        shape = 16,
+        linewidth = 0
+      )
+    )
+  ) +
+  
+  labs(
+    x = "Total Signature Size",
+    y = "Harmonic Mean AUC",
+    color = NULL
+  ) +
+  
+  theme_classic(base_size = 16) +
+  
+  theme(
+    legend.position = "right",
+    axis.title = element_text(
+      face = "bold"),
+    axis.text.x = element_text(
+      angle = 45,
+      hjust = 1
+    )
+    
+  ) +
+  
+  geom_vline(
+    data = vline_df,
+    aes(xintercept = Signature_Size),
+    linetype = "dashed"
+  )
+
+###################################################################################
+###################################################################################
+###################################################################################
